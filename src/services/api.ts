a9533dabac2bffdbc19057912413b0e455cb59b6
Abbade <gabbade@gmail.com>
2022-10-10 02:47:25 -0300

    url nova

--- src/services/api.ts
+++ src/services/api.ts
@@ -13,7 +13,7 @@ export function setupAPIClient(ctx = undefined) {
   let cookies = parseCookies(ctx);
 
   const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: 'https://plantaqui.herokuapp.com',
     headers: {
       Authorization: `Bearer ${cookies['nextauth.token']}`
     }
