0686cb2457735d3a84bf2e513679745ae25ff3db
Gabriel Abbade <gabbade@gmail.com>
2022-10-28 20:43:03 -0300

    autocomplete + roles

--- src/components/Forms/RoleForm.tsx
+++ src/components/Forms/RoleForm.tsx
@@ -1,24 +1,35 @@
-import { Button, createTheme } from "@mui/material";
-import  Box from "@mui/material/Box";
-import  Grid from "@mui/material/Grid";
-import * as yup from "yup";
-import Router from "next/router";
-import { useContext, useEffect } from "react";
-import { api } from "../../services/apiClient";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { Button, createTheme } from "@mui/material";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Router from "next/router";
+import { useContext, useEffect, useState } from "react";
 import { FieldError, SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
 import { AlertContext } from "../../contexts/AlertContext";
 import { EditInterface } from "../../interfaces/EditInterface";
+import { api } from "../../services/apiClient";
+import BasicAutocomplete from "../Inputs/BasicAutocompleteMultiple";
 import BasicTextField from "../Inputs/BasicTextField";
 
 type CreateFormData = {
   name: string;
   id?: number;
+  permissions?: PermissoesData[];
+};
+type PermissoesData = {
+  id: number;
+  code: string;
+  name: string;
 };
 
 const createObjFormSchema = yup.object().shape({
   id: yup.number(),
   name: yup.string().required("Nome é obrigatório"),
+  permissions: yup
+    .array()
+    .required("Permissao é obrigatória")
+    .min(1, "Permissão é Obrigatória"),
 });
 
 const theme = createTheme();
@@ -33,6 +44,16 @@ export default function RoleForm({ id }: EditInterface) {
   } = useForm({ resolver: yupResolver(createObjFormSchema) });
 
   const { showAlert } = useContext(AlertContext);
+  const [permissions, setPermissions] = useState([] as PermissoesData[]);
+
+  useEffect(() => {
+    const get = async () => {
+      const permissions = await api.get("/permissions");
+      setPermissions(permissions.data.itens);
+    };
+
+    get();
+  }, []);
 
   useEffect(() => {
     const get = async (id) => {
@@ -40,14 +61,14 @@ export default function RoleForm({ id }: EditInterface) {
         const item = await api.get(`roles/${id}`);
         setValue("name", item.data.name);
         setValue("id", item.data.id);
+        setValue("permissions", item.data.permissions);
       }
     };
+
     get(id);
   }, [id]);
 
-  const handleLoteSubmit: SubmitHandler<CreateFormData> = async (
-    formData
-  ) => {
+  const handleLoteSubmit: SubmitHandler<CreateFormData> = async (formData) => {
     try {
       if (formData.id > 0) {
         const item = await api.put("roles", formData);
@@ -56,10 +77,9 @@ export default function RoleForm({ id }: EditInterface) {
         const item = await api.post("roles", formData);
         showAlert("Perfil cadastrado com sucesso.", "success");
       }
-
       Router.back();
     } catch (error) {
-      const errorOficial = error as Error;
+      showAlert(error.response.data.message, "error");
     }
   };
 
@@ -77,6 +97,15 @@ export default function RoleForm({ id }: EditInterface) {
             name={"name"}
             control={control}
             error={errors.name as FieldError}
+          />
+        </Grid>
+        <Grid item xs={12} sm={12}>
+          <BasicAutocomplete
+            control={control}
+            label={"Permissões"}
+            name={"permissions"}
+            error={errors.permissions as FieldError}
+            options={permissions}
           />
         </Grid>
       </Grid>
---
+++ src/components/Inputs/BasicAutocompleteMultiple.tsx
@@ -0,0 +1,57 @@
+import Autocomplete from "@mui/material/Autocomplete";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Control, Controller, FieldError, FieldValues } from "react-hook-form";
+
+interface AutocompleteParams {
+  label: string;
+  name: string;
+  // values?: any[];
+  options?: any[];
+  error?: FieldError;
+  control: Control<FieldValues, any>;
+}
+
+export default function BasicAutocompleteMultiple({
+  label,
+  name,
+  //values,
+  control,
+  options,
+  error = null,
+}: AutocompleteParams) {
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <Controller
+        control={control}
+        name={name}
+        // defaultValue={[{}]}
+        render={({ field: { ref, onChange, value = [], ...field } }) => {
+          return (
+            <Autocomplete
+              multiple
+              options={options}
+              defaultValue={value}
+              value={value}
+              isOptionEqualToValue={(option, value) => option.id === value.id}
+              getOptionLabel={(option) => option.name}
+              onChange={(_, data) => onChange(data)}
+              renderInput={(params) => (
+                <TextField
+                  {...field}
+                  {...params}
+                  fullWidth
+                  inputRef={ref}
+                  variant="filled"
+                  error={!!error}
+                  helperText={error?.message}
+                  label={label}
+                />
+              )}
+            />
+          );
+        }}
+      />
+    </Box>
+  );
+}
