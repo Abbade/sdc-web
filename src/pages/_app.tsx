7c30bbc16056078cbf83177a601fbafc073e6e67
Abbade <gabbade@gmail.com>
2022-11-04 01:46:30 -0300

    ultimo theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -29,6 +29,7 @@ const LayoutComponent = isLayoutNeeded ? Layout : React.Fragment;
       <Theme >
       <AlertProvider>
       <Head>
+        <ThemeColor />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
    
